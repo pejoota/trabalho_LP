73c746b736f8272c3a6bc200711733c7e9b624c6
hyagohoudini <oicueio616@gmail.com>
2021-10-01 18:15:08 -0300

    Versão inicial para Controller/ingredients

--- api/src/controllers/ingredients.go
+++ api/src/controllers/ingredients.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
 
 //Insere usuário no db
 func CreateIngredient(w http.ResponseWriter, r *http.Request) {
@@ -14,15 +19,66 @@ func GetAllIngredients(w http.ResponseWriter, r *http.Request) {
 
 //Busca um usuário salvo no db
 func GetIngredientById(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Buscando o GetIngredientById!`${ingredient_id}`"))
+	w.Header().Set("Content-Type", "aplication/json")
+	partes := strings.Split(r.URL.Path,"/")
+
+	if len(partes) > 3 {
+		//w.WriteHeader(http.StatusNotFound)
+		w.WriteHeader(400)
+		return
+	}
+
+	id, err := strconv.Atoi(partes[2])
+	if err != nil {
+		w.WriteHeader(404)
+		return 
+	}
+
+	w.WriteHeader(200)
+
+	fmt.Println("ingredient_id:", id," foi encontrado")
 }
 
 //Atualiza informações de um usuário e salva no db
 func UpdateIngredientById(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Atualizando ingredient_id!"))
+	w.Header().Set("Content-Type", "aplication/json")
+	partes := strings.Split(r.URL.Path,"/")
+
+	if len(partes) > 3 {
+		//w.WriteHeader(http.StatusNotFound)
+		w.WriteHeader(400)
+		return
+	}
+
+	id, err := strconv.Atoi(partes[2])
+	if err != nil {
+		w.WriteHeader(404)
+		return 
+	}
+
+	w.WriteHeader(200)
+
+	fmt.Println("ingredient_id:", id," foi Atualizado")
 }
 
 //Apaga usuário do db
 func DeleteIngredientById(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Deletando ingredient_id!"))
+	w.Header().Set("Content-Type", "aplication/json")
+	partes := strings.Split(r.URL.Path,"/")
+
+	if len(partes) > 3 {
+		//w.WriteHeader(http.StatusNotFound)
+		w.WriteHeader(400)
+		return
+	}
+
+	id, err := strconv.Atoi(partes[2])
+	if err != nil {
+		w.WriteHeader(404)
+		return 
+	}
+
+	w.WriteHeader(200)
+
+	fmt.Println("ingredient_id:", id," foi deletado")
 }
